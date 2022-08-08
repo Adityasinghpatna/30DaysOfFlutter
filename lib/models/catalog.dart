acbcec0db0b67366112434bf4f9d9777e331753c
Aditya Singh <adisinghnbt@gmail.com>
2022-08-08 23:43:46 +0530

    Day 10 completed

---
+++ lib/models/catalog.dart
@@ -0,0 +1,21 @@
+class item{
+  final String id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  item(this.id, this.name, this.desc, this.price, this.color, this.image);
+}
+
+final products = [
+  Item(
+  id: "Aditya001",
+  name: "Redmi note 5 pro",
+  desc:"Redmi note 5 pro 9th generation",
+  price:999,
+  color:"#33505a",
+  image:"C:\Users\HP\Downloads\login_image.png"
+),
+];
