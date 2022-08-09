a851c37a6a08069f298781fe49e35e61cf88bc35
Aditya Singh <adisinghnbt@gmail.com>
2022-08-09 14:46:02 +0530

    day 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,21 +1,29 @@
-class item{
-  final String id;
+class CatalogModel {
+  static final items = [
+    Item(
+      id: 1,
+      name: "Redmi note 5 pro",
+      desc: "Redmi note 5 pro 9th generation",
+      price: 999,
+      color: "#33505a",
+      image: "https://picsum.photos/200/300",
+    )
+  ];
+}
+
+class Item {
+  final int id;
   final String name;
   final String desc;
   final num price;
   final String color;
   final String image;
 
-  item(this.id, this.name, this.desc, this.price, this.color, this.image);
-}
-
-final products = [
   Item(
-  id: "Aditya001",
-  name: "Redmi note 5 pro",
-  desc:"Redmi note 5 pro 9th generation",
-  price:999,
-  color:"#33505a",
-  image:"C:\Users\HP\Downloads\login_image.png"
-),
-];
+      {required this.id,
+      required this.name,
+      required this.desc,
+      required this.price,
+      required this.color,
+      required this.image});
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,23 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/pages/widgets/drawer.dart';
+import 'package:flutter_application_1/pages/widgets/item_widget.dart';
 
-//* Day 11 we learned about context,constraints
 class HomePage extends StatelessWidget {
- final int days = 30;
-   final String name = "Aditya";
- @override
+  final int days = 30;
+  final String name = "Aditya";
+  @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       drawer: Mydrawer(),
-      appBar:AppBar(
-        title: Text("Catalog App",
-        style: TextStyle(color: Colors.black), ),
-         ),
-        body:Center(
-          child:Container(
-          child:Text("Welcome to $days days of flutter by $name"),
-          ),
-        )   
-      );
+      appBar: AppBar(
+        title: Text(
+          "Catalog App",
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+              key: UniqueKey(),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
