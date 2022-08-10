57539ded8d0f43deee019554db78d111c4f12431
Aditya Singh <adisinghnbt@gmail.com>
2022-08-10 16:33:23 +0530

    day 13 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,11 +1,38 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:convert';
 import 'package:flutter_application_1/models/catalog.dart';
 import 'package:flutter_application_1/pages/widgets/drawer.dart';
 import 'package:flutter_application_1/pages/widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final int days = 30;
+
   final String name = "Aditya";
+
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+
+  loadData() async {
+  final catalogJson = await rootBundle.loadString("assets/files/catalog.json");
+  final decodeData = jsonDecode(catalogJson);
+  var productsData = decodeData["products"];
+  print(productsData);
+  }
+
+  
+
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
@@ -31,4 +58,5 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+  
 }
