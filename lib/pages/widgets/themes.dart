274e65cb33dfc47b6364f1c0abe861a2052d8ebe
Aditya Singh <adisinghnbt@gmail.com>
2022-08-12 18:52:11 +0530

    Day 17 completed

--- lib/pages/widgets/themes.dart
+++ lib/pages/widgets/themes.dart
@@ -1,26 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MyTheme{
+class MyTheme {
   static ThemeData lighttheme(BuildContext context) => ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        appBarTheme: AppBarTheme(
-          color: Colors.white,
-          elevation: 0.0,
-          iconTheme: IconThemeData(color: Colors.black),
-         textTheme:Theme.of(context).textTheme, 
-        )
+      primarySwatch: Colors.deepPurple,
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 0.0,
+        iconTheme: IconThemeData(color: Colors.black),
+        textTheme: Theme.of(context).textTheme,
+      ));
+
+  static ThemeData darktheme(BuildContext context) => ThemeData(
+        brightness: Brightness.dark,
       );
 
-       static ThemeData darktheme(BuildContext context) => ThemeData(
-        primarySwatch: Colors.deepPurple,
-        fontFamily: GoogleFonts.lato().fontFamily,
-        appBarTheme: AppBarTheme(
-          color: Colors.white,
-          elevation: 0.0,
-          iconTheme: IconThemeData(color: Colors.black),
-         textTheme:Theme.of(context).textTheme, 
-        )
-      );
+  //Colours
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
 }
