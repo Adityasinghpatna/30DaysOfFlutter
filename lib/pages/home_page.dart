6124dd26f86d83f1a771bd5187a4aec112703168
Aditya Singh <adisinghnbt@gmail.com>
2022-08-12 11:44:59 +0530

    Day 16 completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,7 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final int days = 30;
 
-  final String name = "Aditya";
+  final String name = "ADITYA";
 
   @override
   void initState() {
@@ -48,18 +48,48 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
-            ? ListView.builder(
+            ? GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 16,
+                crossAxisSpacing: 16,
+                ),
                 itemCount: CatalogModel.items!.length,
-                itemBuilder: (context, index) =>
-                   ItemWidget(
-                    item: CatalogModel.items![index],
-                    key: UniqueKey(),
-                  ),
-              )
-            : Center(
+               itemBuilder: (context, index) {
+                final item = CatalogModel.items![index];
+                 return Card(
+                  clipBehavior: Clip.antiAlias,
+                 shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)), 
+                  child: GridTile(
+                    header: Container(child: Text(
+                      item.name,
+                      style: TextStyle(color: Colors.white),
+                      ),
+                    padding:const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple,
+                    ),
+                    ),
+                    child: Image.network(item.image),
+                    footer:Container(child: Text(
+                      item.price.toString(),
+                      style: TextStyle(color: Colors.white),
+                      ),
+                    padding:const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                    ),
+                    ),
+                  ));
+                  
+               },
+               
+               ) : Container(),
+               )
+              );
+              Center(
                 child: CircularProgressIndicator(),
-              ),
-      ),
-    );
+              );
   }
 }
